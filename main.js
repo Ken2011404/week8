1ddf07e1dfcb9c91c1d4ae7db0237e8631ef0dac
ken2011404 <ken2011404@hotmail.com>
2025-08-24 21:54:46 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+console.log("Hello world");
+
+const swiper = new Swiper(".swiper", {
+  //間距24px
+  spaceBetween: 16,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  // 左右箭頭
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  //呈現三個
+  slidesPerView: 3,
+  // 滾動條
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  autoplay: true,
+
+  // 響應式設定（類似 Bootstrap 的斷點）
+  breakpoints: {
+    // 0px 以上（手機）
+    0: {
+      slidesPerView: 1,
+    },
+    // 768px 以上（平板）
+    768: {
+      slidesPerView: 2,
+    },
+    // 992px 以上（桌機）
+    992: {
+      slidesPerView: 3,
+    },
+  },
+});
